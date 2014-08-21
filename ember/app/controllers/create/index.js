e71296c6aa9916a9d6c72f8caee046b4a13b30b0
Robert DeLuca <robertd@izea.com>
2014-08-20 22:03:48 -0400

    Updated to canary and format time

--- ember/app/controllers/create/index.js
+++ ember/app/controllers/create/index.js
@@ -1,31 +1,33 @@
-import Ember from 'ember';
+import Ember from "ember";
 
 var CreateController = Ember.ObjectController.extend({
 
   init: function() {
-    this.set('post',  Ember.Object.create());
+    this.set("post",  Ember.Object.create());
   },
 
   actions: {
 
     publishPost: function() {
+      var date = new Date().getTime(),
+          postData = {
+            title: this.get("post.title"),
+            excerpt: this.get("post.excerpt"),
+            body: this.get("post.body"),
+            post_slug: this.get("post.title").replace(/\s+$/g,'').replace(/\s+/g, '-').toLowerCase()
+          };
+      console.log(postData);
 
-      var newPost = this.store.createRecord('post', {
-        title: this.get('post.title'),
-        excerpt: this.get('post.excerpt'),
-        body: this.get('post.body'),
-        published: new Date().getTime(),
-        post_slug: this.get("post.title").replace(/\s+$/g,'').replace(/\s+/g, '-').toLowerCase()
-      });
+      var newPost = this.store.createRecord("post", postData);
 
       newPost.save();
 
       this.setProperties({
-        'post.title': '',
-        'post.excerpt': '',
-        'post.body': ''
+        "post.title": "",
+        "post.excerpt": "",
+        "post.body": ""
       });
-      this.transitionToRoute('posts'); // Do I want this, or the post that was just made?
+      this.transitionToRoute("posts"); // Do I want this, or the post that was just made?
     }
   }
 
--- ember/app/models/posts.js
+++ ember/app/models/posts.js
@@ -3,10 +3,10 @@ import DS from "ember-data";
 export default DS.Model.extend({
   post_slug: DS.attr("string"),
   title: DS.attr("string"),
-  published_date: DS.attr("date"),
-  formatted_date: function() {
-    return moment(this.get("published_date")).format("MMM Do");
-  }.property("published_date"),
+  created_at: DS.attr("date"),
   excerpt: DS.attr("string"),
-  body: DS.attr("string")
+  body: DS.attr("string"),
+  formatted_date: function() {
+    return moment(this.get("created_at")).format("MMM Do");
+  }.property("created_at")
 });
