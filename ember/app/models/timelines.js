512d55eb665e7d2a380f6c2d1c1ced5f66c52474
Robert DeLuca <robertd@izea.com>
2014-10-05 14:27:17 -0400

    Working on front end of timeline

---
+++ ember/app/models/timelines.js
@@ -0,0 +1,8 @@
+import DS from 'ember-data';
+
+export default DS.Model.extend({
+  title: DS.attr("string"),
+  description: DS.attr("string"),
+  date: DS.attr("date"),
+  created_at: DS.attr("date")
+});
--- ember/app/router.js
+++ ember/app/router.js
@@ -5,14 +5,17 @@ var Router = Ember.Router.extend({
 });
 
 Router.map(function() {
-  this.route("about");
   this.route("portfolio");
   this.route("login");
   this.route("dashboard");
   this.resource("posts", function() {
-    this.route("show", {path: "/:post_slug"});
-    this.route("edit", {path: "/:post_slug/edit"});
+    this.route("show", { path: "/:post_slug" });
+    this.route("edit", { path: "/:post_slug/edit" });
     this.route("new");
+  });
+  this.resource("about", function(){
+    this.route("new");
+    this.route("edit", { path: "/:timeline_id/edit" });
   });
   this.route('error404', { path: '/*path' }); //404s son
 });
