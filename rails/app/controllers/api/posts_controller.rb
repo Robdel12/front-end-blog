6e648abc9a03bdcfbf89ef66b0860d2bab4b0b4a
Robert DeLuca <robertd@izea.com>
2015-01-21 10:30:23 -0500

    Fixed markdown stuff and ordered the dashboard

--- rails/app/controllers/api/posts_controller.rb
+++ rails/app/controllers/api/posts_controller.rb
@@ -3,7 +3,7 @@ class Api::PostsController < ApplicationController
 
   def index
     if params[:dashboard]
-      render json: Posts.all
+      render json: Posts.all.order("created_at desc")
     else
       page = (params[:page] || 1).to_i
       posts = Posts.where(is_published: true).order("created_at desc").page(page).per(5)
