10b45e99d9ab6c6bf8c5d8e0b588a756dbb4d14a
Robert DeLuca <robertd@izea.com>
2015-02-02 23:23:32 -0500

    Removing first page query param and adding perPage query

--- ember/app/controllers/posts/index.js
+++ ember/app/controllers/posts/index.js
@@ -1,9 +1,11 @@
 import Ember from 'ember';
 
 var PostsController = Ember.ArrayController.extend({
-  queryParams: ["page"],
+  queryParams: ["page", "perPage"],
   pageBinding: "content.page",
-  totalPagesBinding: "content.totalPages"
+  totalPagesBinding: "content.totalPages",
+  page: 1,
+  perPage: 10
 });
 
 export default PostsController;
