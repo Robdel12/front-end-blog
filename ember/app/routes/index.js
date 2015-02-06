652ae8af85ff634b4e70ba82947f72d3f0b9dd24
Robert DeLuca <robertd@izea.com>
2015-02-06 12:47:03 -0500

    Stop redirecting and render posts in root index

--- ember/app/routes/index.js
+++ ember/app/routes/index.js
@@ -1,8 +1,9 @@
 import Ember from 'ember';
+import RouteMixin from 'ember-cli-pagination/remote/route-mixin';
 
-var IndexRoute = Ember.Route.extend({
-  redirect: function() {
-    this.replaceWith("posts");
+var IndexRoute = Ember.Route.extend(RouteMixin, {
+  model: function(params) {
+    return this.findPaged("post", params);
   }
 });
 
