e76abe61c6ce0853a35d418d050cb1a8aa44ca3e
Robdel12 <robertdeluca19@gmail.com>
2015-05-25 01:19:30 -0500

    Update ENV to fix tests

--- config/environment.js
+++ config/environment.js
@@ -33,6 +33,7 @@ module.exports = function(environment) {
     // ENV.APP.LOG_TRANSITIONS = true;
     // ENV.APP.LOG_TRANSITIONS_INTERNAL = true;
     // ENV.APP.LOG_VIEW_LOOKUPS = true;
+    ENV.apiUrl = null;
   }
 
   if (environment === 'test') {
@@ -48,6 +49,7 @@ module.exports = function(environment) {
     ENV.APP.LOG_VIEW_LOOKUPS = false;
 
     ENV.APP.rootElement = '#ember-testing';
+    ENV.apiUrl = null;
 
   }
 
