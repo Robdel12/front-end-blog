206c5bfa9a8a94b0495b35cdb35facd61ccc2a31
Robdel12 <robertdeluca19@gmail.com>
2015-06-18 22:53:56 -0500

    Cleaning up code and creating a better way to autosave

--- app/controllers/posts/base.js
+++ app/controllers/posts/base.js
@@ -1,30 +1,32 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
-  published: [false, true],
   isPreviewing: false,
-  settings: {
-    mobile: true
-  },
 
-  setUrl: function() {
+  setUrl: Ember.observer('model.title', function() {
     if(!this.get("model.title")){ return false; }
 
     var currentPostSlug = this.get("model.title").replace(/\W/g,'-').replace(/-{1,}/g,'-').replace(/^-|-$/g,'').toLowerCase();
 
     return this.set("model.postSlug", currentPostSlug);
-  }.observes("model.title"),
+  }),
 
   actions: {
     save: function() {
 
-      this.get("model").save().catch(function(reason) {
+      this.get("model").save().then(() => {
+        return this.transitionTo('posts.edit', this.get('model'));
+      }).catch(function(reason) {
         if(reason.status === 500) {
           this.get('flashes').danger("There was a server error.");
         }
       });
 
       return false;
+    },
+
+    selectPublished: function(selection) {
+      this.set('model.isPublished', selection);
     },
 
     togglePreview: function() {
--- app/routes/posts/edit.js
+++ app/routes/posts/edit.js
@@ -1,16 +1,10 @@
 import Ember from "ember";
 import AuthenticatedRouteMixin from "simple-auth/mixins/authenticated-route-mixin";
 
-var PostsRoute = Ember.Route.extend(AuthenticatedRouteMixin, {
+export default Ember.Route.extend(AuthenticatedRouteMixin, {
 
   model: function(params) {
     return this.store.find("post", params.post_id);
-  },
-
-  deactivate: function(){
-    this.controllerFor(this.routeName).stopAutoSave();
   }
 
 });
-
-export default PostsRoute;
