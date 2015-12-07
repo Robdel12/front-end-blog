0c20e15215c3a9b94c1d62dabd58bdfb6b005d47
Robdel12 <robertdeluca19@gmail.com>
2015-12-07 15:10:55 -0600

    Update project

--- tests/helpers/start-app.js
+++ tests/helpers/start-app.js
@@ -1,7 +1,6 @@
 import Ember from 'ember';
 import registerA11yHelpers from './a11y/register-a11y-helpers';
 import Application from '../../app';
-import Router from '../../router';
 import config from '../../config/environment';
 import initializeTestHelpers from 'simple-auth-testing/test-helpers';
 
