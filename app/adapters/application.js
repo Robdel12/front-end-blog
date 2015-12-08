cb5b17e59749b5c29db098a8c866b0928198193e
Robdel12 <robertdeluca19@gmail.com>
2015-12-07 18:16:23 -0600

    WIP: Updating simple auth

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -1,13 +1,15 @@
 import Ember from "ember";
 import config from '../config/environment';
 import ActiveModelAdapter from 'active-model-adapter';
+import DataAdapterMixin from 'ember-simple-auth/mixins/data-adapter-mixin';
 
 var inflector = Ember.Inflector.inflector;
 inflector.uncountable('timeline'); //only makes call to /timeline
 
-export default ActiveModelAdapter.extend({
+export default ActiveModelAdapter.extend(DataAdapterMixin, {
   namespace: "api",
   host: config.apiUrl,
+  authorizer: 'authorizer:application',
   shouldReloadAll() {
     return true;
   }
