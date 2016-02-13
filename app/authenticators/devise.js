bc4589fa987c614fc5ca1f578822e5787747c6ba
Robdel12 <robertdeluca19@gmail.com>
2016-02-12 23:57:39 -0600

    Random updates

--- app/authenticators/devise.js
+++ app/authenticators/devise.js
@@ -1,5 +1,13 @@
 import Devise from 'ember-simple-auth/authenticators/devise';
+import env from 'front-end/config/environment';
+import Ember from 'ember';
 
 export default Devise.extend({
-
+  serverTokenEndpoint: Ember.computed(function() {
+    if(env.environment !== 'development') {
+      return `${env.apiUrl}/users/sign_in`;
+    } else {
+      return `/users/sign_in`;
+    }
+  })
 });
