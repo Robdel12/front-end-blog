4a4bfa6b3027836301cc66974ff285fd28084a1c
Robdel12 <robertdeluca19@gmail.com>
2016-08-14 17:54:01 -0500

    Update ember data & fix tests for mirage

--- mirage/config.js
+++ mirage/config.js
@@ -28,12 +28,12 @@ export default function() {
     this.get('/contacts/:id', ['contact', 'addresses']);
    */
 
-  this.post('/api/timeline', function(db, request) {
+  this.post('/api/timeline', function(schema, request) {
     return JSON.parse(request.requestBody);
   });
 
-  this.get('/api/timeline', function(db) {
-    return { timeline: db.timeline };
+  this.get('/api/timeline', function(schema) {
+    return { timeline: schema.db.timeline };
   });
 
   /*
