dd24900705a4cb33bf6870d94bc9fa248214afbd
Robdel12 <robertdeluca19@gmail.com>
2016-08-14 17:21:45 -0500

    Silence deprecation warnings

--- config/deprecation-workflow.js
+++ config/deprecation-workflow.js
@@ -1,8 +1,8 @@
 window.deprecationWorkflow = window.deprecationWorkflow || {};
 window.deprecationWorkflow.config = {
   workflow: [
-    { handler: "silence", matchId: "ember-application.app-initializer-initialize-arguments" },
-    { handler: "silence", matchId: "ember-application.injected-container" },
-    { handler: "silence", matchId: "ember-application.app-instance-container" }
+    { handler: "throw", matchId: "ember-application.app-initializer-initialize-arguments" },
+    { handler: "throw", matchId: "ember-application.injected-container" },
+    { handler: "throw", matchId: "ember-application.app-instance-container" }
   ]
 };
